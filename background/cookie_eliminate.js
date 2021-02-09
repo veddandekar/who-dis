1fdabeeb95f829f5fa21d4b2dda5616a2e97e4b7
Ved Dandekar <veddandekar6@gmail.com>
2021-02-09 13:56:45 +0530

    feat: badges in icon

--- background/cookie_eliminate.js
+++ background/cookie_eliminate.js
@@ -1,4 +1,19 @@
 // Called when the user clicks on the browser action.
+chrome.tabs.onActivated.addListener((info) => {
+	console.log(info);
+	chrome.tabs.get(info.tabId, (tab) => {
+		if (!("url" in tab)) return;
+		console.log(tab.url);
+		let url = new URL(tab.url);
+		let domain = "." + url.hostname;
+		chrome.cookies.getAll({ domain }, (coks) => {
+			chrome.browserAction.setBadgeText({
+				text: coks.length.toString(),
+			});
+		});
+	});
+});
+
 chrome.browserAction.onClicked.addListener(function (tab) {
 	let url = new URL(tab.url);
 	let domain = "." + url.hostname;
